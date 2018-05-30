2807f6f87f358cc25e03b5cd160f6edcefe0f9fc
malai001 <annamalai.vignesh7@gmail.com>
2018-05-30 22:58:34 +0530

    django_app sample blog

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Post
+# Register your models here.
+
+admin.site.register(Post)
+#72b3b559d4bc36f3e0d59e649764d41895daa7a6
